e7b09c566a7b15ce6cd48a8528f270efbd13a93e
Nikolas Wise <nikolaswise@gmail.com>
2023-02-09 15:34:38 -0800

    auth the access to triplestores

--- src/lib/ld/query.js
+++ src/lib/ld/query.js
@@ -1,13 +1,14 @@
-import { sparql_endpoint } from '$env/static/private'
+import { sparql_endpoint, username, password } from '$env/static/private'
 import jsonld from 'jsonld'
 import context from '$lib/ld/context'
 import prefixes from '$lib/ld/prefixes'
 
+const headers = new Headers()
+headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
+
 const getTriples = (accept) => async (query) => await fetch(`${sparql_endpoint}/query`, {
   method: 'POST',
-  headers:{
-    'Accept': accept
-  },
+  headers: headers,
   body: new URLSearchParams({
     'query': `${prefixes}
     ${query}`
@@ -17,8 +18,10 @@ const getTriples = (accept) => async (query) => await fetch(`${sparql_endpoint}/
 export const queryJSON = async query => {
   let triples = await getTriples('application/n-triples')(query)
     .then(result => result.text())
+
   const doc = await jsonld.fromRDF(triples, {format: 'application/n-quads'});
   const compact = await jsonld.compact(doc, context)
+
   delete compact['@context']
   if (compact['@graph']) {
     return compact['@graph']
@@ -35,12 +38,20 @@ export const queryArr = async query => {
   return triples
 }
 
+export const ask = async query => {
+  let triples = await getTriples('application/sparql-results+json')(query)
+      .then(result => result.json())
+  return triples.boolean
+}
+
 export const update = async update => {
   await fetch(`${sparql_endpoint}/update`, {
     method: 'POST',
-    headers:{
+    headers: {
+      'Authorization': 'Basic ' + btoa(username + ":" + password),
       'Content-Type': 'application/x-www-form-urlencoded'
     },
+
     body: new URLSearchParams({
       'update': `${prefixes}
       DELETE {
